f826c90f6d3a7ed0d41a3ba0248c112942699212
Mathieu Boisnard <mathieu.boisnard@lacombedulionvert.fr>
2020-11-30 14:18:37 +0100

    Check if plugin is correctly deployed in Keycloak instance
    Signed-off-by: Mathieu Boisnard <mathieu.boisnard@lacombedulionvert.fr>

--- integration-tests/src/test/java/fr/insee/keycloak/FranceConnectPluginTest.java
+++ integration-tests/src/test/java/fr/insee/keycloak/FranceConnectPluginTest.java
@@ -1,14 +1,47 @@
 package fr.insee.keycloak;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.Container;
+import org.testcontainers.utility.MountableFile;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FranceConnectPluginTest {
 
-    @Test
-    void should_start_keycloak_container_with_installed_france_connect_plugin() {
+    private static final String PLUGIN_FILE = "keycloak-franceconnect.jar";
 
-        KeycloakContainer keycloakContainer = new KeycloakContainer();
+    private static final String PLUGIN_FILE_PATH_IN_HOST = "../plugin/target/" + PLUGIN_FILE;
+
+    private static final String PLUGINS_FOLDER_IN_CONTAINER = "/opt/jboss/keycloak/standalone/deployments/";
+    private static final String PLUGIN_FILE_PATH_IN_CONTAINER = PLUGINS_FOLDER_IN_CONTAINER + PLUGIN_FILE;
+
+    private static final String PLUGIN_SUCCESS_DEPLOYMENT_FILE = PLUGIN_FILE + ".deployed";
+
+    private static KeycloakContainer keycloakContainer;
+
+    @BeforeAll
+    static void setup() {
+        keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:9.0.2")
+            .withCopyFileToContainer(MountableFile.forHostPath(PLUGIN_FILE_PATH_IN_HOST), PLUGIN_FILE_PATH_IN_CONTAINER);
+
         keycloakContainer.start();
+    }
+
+    @Test
+    void check_if_france_connect_plugin_is_correctly_deployed() throws IOException, InterruptedException {
+        assertThat(keycloakContainer.isRunning()).isTrue();
+
+        Container.ExecResult commandResult = keycloakContainer.execInContainer(
+            "cat", PLUGINS_FOLDER_IN_CONTAINER + PLUGIN_SUCCESS_DEPLOYMENT_FILE
+        );
+
+        assertThat(commandResult.getExitCode()).isZero();
+
+        String keycloakLogs = keycloakContainer.getLogs();
+        assertThat(keycloakLogs).contains("Deployed \"" + PLUGIN_FILE + "\"");
     }
 }
